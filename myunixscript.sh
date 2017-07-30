20ee54e0ee1517d93304d6ab96b022131bfb5b50
shekar-thandava <30564361+shekar-thandava@users.noreply.github.com>
2017-07-30 19:28:12 +0800

    changes 4

--- myunixscript.sh
+++ myunixscript.sh
@@ -6,3 +6,5 @@ echo "hello, today is $(date)" >> /home/cloudera/shekar-cdh/jenkins_learn/jenkin
 echo "GitHub working at $(date)!!!" >> /home/cloudera/shekar-cdh/jenkins_learn/jenkins_test
 
 echo "Quite a fruitful day!!" >> /home/cloudera/shekar-cdh/jenkins_learn/jenkins_test
+
+echo "Close for day!!" >> /home/cloudera/shekar-cdh/jenkins_learn/jenkins_test
