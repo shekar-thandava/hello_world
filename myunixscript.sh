d79876dd0123f1526f9ea470c99e9a59587b25aa
shekar-thandava <30564361+shekar-thandava@users.noreply.github.com>
2017-07-30 18:48:05 +0800

    changes 1

--- myunixscript.sh
+++ myunixscript.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo "hello, today is $(date)" >> /home/cloudera/shekar-cdh/jenkins_learn/jenkins_test
+
+
+echo "GitHub working at $(date)" >> /home/cloudera/shekar-cdh/jenkins_learn/jenkins_test
